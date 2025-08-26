47a434d62bece439b7189b785fe6b39e4720b651
unnati10mishra-prog <unnati10mishra@gmail.com>
2025-08-26 14:25:19 +0530

    Create 1. Print name, course, and university.c

---
+++ 1. Print name, course, and university.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main() {
+    printf("Name: Unnati Mishra\n");
+    printf("Course: Computer Science Engineering\n");
+    printf("University: Chandigarh University, Uttar Pradesh\n");
+    return 0;
+}
