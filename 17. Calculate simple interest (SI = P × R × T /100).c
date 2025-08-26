89865112919d6d866b282825d7623928609c067c
unnati10mishra-prog <unnati10mishra@gmail.com>
2025-08-26 14:53:22 +0530

    Delete 17. Calculate simple interest (SI = P × R × T  directory

--- 17. Calculate simple interest (SI = P × R × T /100).c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    float P, R, T, SI;
-    printf("Enter principal, rate and time: ");
-    scanf("%f %f %f", &P, &R, &T);
-    SI = (P * R * T) / 100;
-    printf("Simple Interest = %.2f\n", SI);
-    return 0;
-}
